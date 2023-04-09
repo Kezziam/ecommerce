348f356b3c7d3371d8708e8899272a8512d829cc
kezzaim <karim.ezzaime@gmail.com>
2023-04-09 03:02:41 +0000

    add function to admin

--- src/main/java/cigma/mini/project/ecommerce/controller/AdminController.java
+++ src/main/java/cigma/mini/project/ecommerce/controller/AdminController.java
@@ -5,10 +5,18 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/admin")
 @CrossOrigin
 public interface AdminController {
+
+    List<ArticleVo> getAllArticle();
+
+    List<ArticleVo> getArticleByDescriptionLike(String word);
+
+    ArticleVo getArticleByReference(String reference);
 
     ArticleVo saveArticle(ArticleVo articleVo);
 
