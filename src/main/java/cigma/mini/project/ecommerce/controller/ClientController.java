4d2e7e5912f4c7a6e02e3d7ec4f6eaf8c22f68da
kezzaim <karim.ezzaime@gmail.com>
2023-04-09 02:47:16 +0000

    add annotation rest controller and url mapping

--- src/main/java/cigma/mini/project/ecommerce/controller/ClientController.java
+++ src/main/java/cigma/mini/project/ecommerce/controller/ClientController.java
@@ -1,9 +1,14 @@
 package cigma.mini.project.ecommerce.controller;
 
 import cigma.mini.project.ecommerce.model.vo.BasketVo;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RestController
+@RequestMapping("/client")
+@CrossOrigin
 public interface ClientController {
 
     List<BasketVo> findAllBasket();
