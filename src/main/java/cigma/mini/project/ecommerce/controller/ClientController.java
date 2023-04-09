13edd13dcdfc90e997de988f17ac6aeffe40d0b1
kezzaim <karim.ezzaime@gmail.com>
2023-04-09 03:00:51 +0000

    add function to client

--- src/main/java/cigma/mini/project/ecommerce/controller/ClientController.java
+++ src/main/java/cigma/mini/project/ecommerce/controller/ClientController.java
@@ -1,6 +1,7 @@
 package cigma.mini.project.ecommerce.controller;
 
 import cigma.mini.project.ecommerce.model.vo.BasketVo;
+import cigma.mini.project.ecommerce.model.vo.OrderLineVo;
 
 import java.util.List;
 
@@ -11,4 +12,16 @@ public interface ClientController {
     BasketVo getBasketById(Long id);
 
     BasketVo updateBasket(BasketVo basketVo);
+
+    OrderLineVo getOrderLineById(Long id);
+
+    List<OrderLineVo> getAllOrderLine();
+
+    OrderLineVo saveOrderLine(OrderLineVo orderLineVo);
+
+    OrderLineVo updateOrderLine(OrderLineVo orderLineVo);
+
+    boolean deleteOrderLine(OrderLineVo orderLineVo);
+
+    boolean deleteAllOrderLineArticleOfClientByClientId(long idClient);
 }
