d36b13550b5dae3a192e90a9ec881916f5d8551d
kezzaim <karim.ezzaime@gmail.com>
2023-04-09 02:57:51 +0000

    remove annotations admin

--- src/main/java/cigma/mini/project/ecommerce/controller/AdminController.java
+++ src/main/java/cigma/mini/project/ecommerce/controller/AdminController.java
@@ -1,13 +1,8 @@
 package cigma.mini.project.ecommerce.controller;
 
 import cigma.mini.project.ecommerce.model.vo.ArticleVo;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/admin")
-@CrossOrigin
+
 public interface AdminController {
 
     ArticleVo saveArticle(ArticleVo articleVo);
