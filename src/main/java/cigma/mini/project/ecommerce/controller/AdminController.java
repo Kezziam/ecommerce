82fa4471a25dc811afff26ef675388ec08f0220a
kezzaim <karim.ezzaime@gmail.com>
2023-04-09 01:31:02 +0000

    add functionn of admin

--- src/main/java/cigma/mini/project/ecommerce/controller/AdminController.java
+++ src/main/java/cigma/mini/project/ecommerce/controller/AdminController.java
@@ -1,7 +1,13 @@
 package cigma.mini.project.ecommerce.controller;
 
+import cigma.mini.project.ecommerce.model.vo.ArticleVo;
+
 public interface AdminController {
 
+    ArticleVo saveArticle(ArticleVo articleVo);
 
+    ArticleVo updateArticle(ArticleVo articleVo);
+
+    boolean deleteArticle(ArticleVo articleVo);
 
 }
